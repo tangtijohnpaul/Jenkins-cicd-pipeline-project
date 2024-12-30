562dc9bbd3808d4b76303df94ad3152bad42dabf
johnpaul tangti <tangti_johnpaul@yahoo.com>
2024-12-30 09:29:31 -0600

    modifications

--- webapp/server/src/main/java/com/example/Greeter.java
+++ webapp/server/src/main/java/com/example/Greeter.java
@@ -17,5 +17,6 @@ public class Greeter {
    */
   public final String greet(final String someone) {
     return String.format("Hello Avinash, %s!", someone);
+    String DBPassword = "Admin@1234";
   }
 }
